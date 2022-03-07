54fb16d41303f9d4bce9587495241aec4c3d411d
Ansley <ansleydsouza@icloud.com>
2022-03-07 15:23:15 +0800

    Swagger doc updates

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
 var swaggerUi = require("swagger-ui-express");
-swaggerDocument = require('./swagger.json');
+const swaggerJsDoc = require('swagger-jsdoc');
 var cors = require("cors");
-var router = express.Router();
 
 const app = express();
+
+const port = process.env.PORT || 8082;
 
 connectDB();
 
@@ -13,14 +14,27 @@ app.use(cors({ origin: true, credentials:true}));
 
 app.use(express.json());
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Contacts REST API',
+            description: "A REST API built with Express and MongoDB. This API provides the ability to CRUD phone numbers and names"
+        },
+    },
+    swagger: '2.0',    basePath: '/v1',    schemes: [
+        'http'
+    ],
+    consumes: [
+        'application/json'
+    ],    produces: [
+        'application/json'
+    ],
+    apis: ['./routes/api/*.js']
+}
+
 app.use(require("./routes/api/contacts"));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api/v1', router);
-app.listen(4000);
-
-app.get('/', (req,res) => res.send('Hello World'));
-
-const port = process.env.PORT || 8082;
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -5,7 +5,15 @@ const Contacts = require('../../models/Contact');
 
 //Application interfaces
 
-//This interface will get the list of all contacts
+/**
+ * @swagger
+ * /records:
+ *   get:
+ *     description: Get All contacts in the database
+ *     responses:
+ *       200:
+ *         description: Returns all the contacts in the database
+ */
 router.get('/records', (req, res) => {
     Contacts.find()
         .then(contacts => res.json(contacts))
@@ -43,34 +51,104 @@ router.get('/paginatedRecords/:page/:searchName?', async(req, res) => {
     }
 });
 
-//This interface will get a single record by id
+/**
+ * @swagger
+ * /record/{id}:
+ *   get:
+ *     parameters:
+ *      - in: path
+ *        name: Contact ID
+ *        required: true
+ *        type: string
+ *        description: The contact ID.
+ *     description: Get a contact by id
+ *     responses:
+ *       200:
+ *         description: Returns the requested contact
+ */
 router.get('/record/:id', (req, res) => {
     Contacts.findById(req.params.id)
         .then(contact => res.json(contact))
         .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
 });
 
-//This interface will add a single contact number and number to the database
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     parameters:
+ *      - in: body
+ *        name: Request Body
+ *        description: New Contact
+ *        schema:
+ *          type: object
+ *          properties:
+ *            contact_name:
+ *              type: string
+ *            contact_number:
+ *              type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
 router.post('/create', (req, res) => {
     Contacts.create(req.body)
         .then(contacts => res.json({msg: 'New contact added successfully'}))
         .catch(err => res.status(404).json({error: 'Unable to add this contact'}));
 });
 
-//This interface will update a record by the id
+/**
+ * @swagger
+ * /update/{id}:
+ *  put:
+ *      parameters:
+ *          - in: path
+ *            name: Contact ID
+ *            required: true
+ *            type: string
+ *            description: The contact to be updated
+ *          - in: body
+ *            name: Request Body
+ *            description: Updated contact details
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: string
+ *                  contact_name:
+ *                      type: string
+ *                  contact_number:
+ *                      type: string
+ *      responses:
+ *          200:
+ *              description: Contact updated successfully
+ */
 router.put('/update/:id', (req, res) => {
     Contacts.findByIdAndUpdate(req.params.id, req.body)
-        .then(book => res.json({ msg: 'Updated successfully' }))
+        .then(contact => res.json({ msg: 'Updated successfully' }))
         .catch(err =>
             res.status(400).json({ error: 'Unable to update the Database' })
         );
 });
 
-//This interface will delete a record by the id
+/**
+ * @swagger
+ * /{id}:
+ *  delete:
+ *      parameters:
+ *      - in: path
+ *        name: Contact ID
+ *        required: true
+ *        type: string
+ *        description: The contact to be deleted
+ *      responses:
+ *          200:
+ *              description: Record deleted successfully
+ */
 router.delete('/:id', (req, res) => {
     Contacts.findByIdAndRemove(req.params.id, req.body)
         .then(book => res.json({ mgs: 'Contact entry deleted successfully' }))
-        .catch(err => res.status(404).json({ error: 'No such a contact' }));
+        .catch(err => res.status(404).json({ error: 'No such contact' }));
 });
 
 module.exports = router;
