1153d3c364bd0f909217a11ab29e83c210502c3f
Ansley <ansleydsouza@icloud.com>
2022-03-07 14:10:15 +0800

    Initial commit - Swagger

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
+var swaggerUi = require("swagger-ui-express");
+swaggerDocument = require('./swagger.json');
 var cors = require("cors");
+var router = express.Router();
 
 const app = express();
 
@@ -11,6 +14,10 @@ app.use(cors({ origin: true, credentials:true}));
 app.use(express.json());
 
 app.use(require("./routes/api/contacts"));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
+app.listen(4000);
 
 app.get('/', (req,res) => res.send('Hello World'));
 
