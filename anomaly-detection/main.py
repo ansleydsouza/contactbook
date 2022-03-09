2fb3d93e9e37af302976877749176e9de02ace7e
Ansley <ansleydsouza@icloud.com>
2022-03-09 14:24:40 +0800

    Updated code to get the server information

--- anomaly-detection/main.py
+++ anomaly-detection/main.py
@@ -1,24 +1,30 @@
 import pymongo
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError, OperationFailure
 import json
 
 
-
-def connect_to_mongo():
-
+def connect_to_mongo_and_get_stats():
     with open('default.json') as config:
         data = json.load(config)
 
     connection_string = data['CONNECTION_STRING']
 
-    client = MongoClient(connection_string)
+    try:
+        client = MongoClient(connection_string)
+    except ConnectionFailure:
+        print("Could not connect to the database")
+        exit(1)
 
-    return client['Contacts']
+    # Get MongoDB Server Information
+    try:
+        server_info = client.server_info()
+    except ServerSelectionTimeoutError:
+        print("Timeout Error when getting server information")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    db = connect_to_mongo()
-    print(db)
+    stats = connect_to_mongo_and_get_stats()
+    print(stats)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
